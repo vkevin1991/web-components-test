331dace564c2ce60ba3579490c8ce4f39128ace2
Kevin Vargas <kvargas@hnsc.net>
2018-09-07 16:57:23 -0600

    Test

---
+++ src/components/myCustomElement.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+    <h1>My Vue Web Component</h1>
+    <div>{{ msg }}</div>
+  </div>
+</template>
+<script>
+export default {
+    props: {
+        msg: String
+    }
+}
+</script>
+
--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
-import App from './App.vue'
+// import wrap from '@vue/web-component-wrapper'
+// import VueWebComponent from "./components/VueWebComponent";
+import myCustomElement from "./components/myCustomElement";
+import VueWebComponent from '../node_modules/vue-web-component/src/index';
 
-Vue.config.productionTip = false
+// const CustomElement = wrap(Vue, VueWebComponent);
+Vue.config.productionTip = false;
+Vue.config.devtools = false;
+customElements.define("my-custom-element", VueWebComponent(myCustomElement));
 
-new Vue({
-  render: h => h(App)
-}).$mount('#app')
+
